75a6580b2c121c67eded5e0224778482a29025b2
Alexander Foley <alexandergfoley@gmail.com>
2024-01-09 12:50:07 -0700

    adding more docs

--- 03-chat-ssh-server-golang/main.go
+++ 03-chat-ssh-server-golang/main.go
@@ -13,6 +13,20 @@ import (
 	"golang.org/x/crypto/ssh/terminal"
 )
 
+/*
+First, we will want to keep track of a few things:
+1. The sessions we have and which rooms they are in. This will help us make sure
+that the user is sending messages to the right room and will also make sure that
+a user leaves their current room before hopping to another room.
+2. The available rooms that we can offer to users. We will be able to show users
+which rooms are available, but also helps us keep track of who is where.
+
+We also define a few supported commands:
+* /enter <room> will allow a user to enter a room
+* /help will show users a help screen
+* /exit will exit the current session
+* /list will show the chat rooms to the user
+*/
 var (
 	sessions       map[ssh.Session]*rooms.Room
 	availableRooms []*rooms.Room
@@ -22,6 +36,10 @@ var (
 	listCmd        = regexp.MustCompile(`^/list.*`)
 )
 
+/*
+We will use this help message when a user inputs an
+unknown /command or if they type /help.
+*/
 func helpMsg() string {
 	return `
 Hello and welcome to the chat server! Please use
@@ -33,6 +51,12 @@ one of the following commands:
 `
 }
 
+/*
+This is a function to filter a struct on some conditions
+and then return the matching structs. For exmaple, we
+can filter all of the rooms in our availableRooms by room
+name, which is what we do below.
+*/
 func filter[T any](s []T, cond func(t T) bool) []T {
 	res := []T{}
 	for _, v := range s {
@@ -43,6 +67,11 @@ func filter[T any](s []T, cond func(t T) bool) []T {
 	return res
 }
 
+/*
+We will call listRooms when a user runs the /list
+command. It will output each of the room's names, separated
+by newlines.
+*/
 func listRooms() string {
 	var sb strings.Builder
 	for _, r := range availableRooms {
@@ -51,6 +80,25 @@ func listRooms() string {
 	return sb.String()
 }
 
+/*
+chat is the main entrypoint to our chat server. First, we open a new
+terminal object for a user's session where the prompt is their username
+followed by a >.
+
+We then read a line from the user with the ReadLine() function. If
+the line starts with a "/", we assume its a slash command and will respond
+with one of the functions we discussed above. The exception is the /enter
+command. When a user tries to enter a room, we will get the room object
+from our availableRooms and call the Enter function for that room which was
+discussed previously (in rooms.go). To recap, that function adds a user to its
+list of registered users and then shows them the entire history of the chatroom.
+If they are already in a room, we first exit the old room before enterring the
+new one.
+
+If the line doesn't start with a "/", we assume that the user is trying to send
+a message. If they are in a room, we simply send a message. If they are not
+in a room, we just show the help command.
+*/
 func chat(s ssh.Session) {
 	term := terminal.NewTerminal(s, fmt.Sprintf("%s > ", s.User()))
 	for {
@@ -87,12 +135,23 @@ func chat(s ssh.Session) {
 					term.Write([]byte((helpMsg())))
 				}
 			} else {
-				sessions[s].SendMessage(s.User(), line)
+				if sessions[s] != nil {
+					sessions[s].SendMessage(s.User(), line)
+				} else {
+					term.Write([]byte((helpMsg())))
+				}
+
 			}
 		}
 	}
 }
 
+/*
+Finally, we just wrap it all together by in our main().
+First, we create three rooms. Next, we tell the ssh library
+to handle new connections with the chat() function. Finally,
+we use ListenAndServe to start the ssh server on port 2222.
+*/
 func main() {
 	availableRooms = []*rooms.Room{
 		&rooms.Room{Name: "a"},
--- 03-chat-ssh-server-golang/rooms/room.go
+++ 03-chat-ssh-server-golang/rooms/room.go
@@ -5,6 +5,25 @@ import (
 	"golang.org/x/crypto/ssh/terminal"
 )
 
+/*
+The Room struct will have a few pieces of data associated with it.
+ 1. The name of the room: This will be a string identifier to show
+    to users when they use the /enter command.
+ 2. The chat history of the room. The room will keep all of its chat
+    history in memory. When a new user joins, it can then show them
+    the entire history of the chatroom so they can feel caught-up.
+ 3. The users currently in the chatroom. This way, the system can
+    notify the users of new messages and keep a record of who is
+    currently logged in to the room.
+
+As previously said, the room needs to keep track of two things:
+users and messages. So, what's in a user? Well, our User struct
+will be comprised of an SSH Session and the associated PTY terminal.
+The SSH Session gives us underlying session information like the
+username while the terminal gives us a nice way to send and
+receive messages from the user. The message struct, on the other hand,
+just needs to keep track of the sending user and the actual message.
+*/
 type Room struct {
 	Name    string
 	History []Message
@@ -21,21 +40,39 @@ type Message struct {
 	Message string
 }
 
-func send(u User, m Message) {
-	raw := m.From + "> " + m.Message + "\n"
-	u.Terminal.Write([]byte(raw))
-}
+/*
+So, now that our room is established, what kind of things should the room
+be able to do? Well, I think our room should allow users to do three things:
+1. Enter the room
+2. Leave the room
+3. Send messages to the room
 
-func (r *Room) SendMessage(from, message string) {
+Let's first look at our Enter function. This is the function that will be
+called when a user uses the /enter command after logging in. We see
+that the first thing we do is make a User object out the users
+session and terminal. The observant reader will notice we don't do
+any checking to see if the user is already in the room. This is mainly
+because we force users to leave rooms before joining new ones, and this
+is enforced one layer up.
 
-	messageObj := Message{From: from, Message: message}
-	r.History = append(r.History, messageObj)
-	for _, u := range r.Users {
-		if (u.Session.User()) != from {
-			send(u, messageObj)
-		}
-	}
-}
+The second thing our Enter function does is add the new user to the
+room's user slice. Then the room sends them a message and sends them
+all of the messages in its history.
+
+Let us turn our attention to leaving a room. Once a user has decided
+that they want to go to another chatroom, they will call the Leave
+function to remove them from this current room. All our leave function
+has to do is remove them from the user slice. Note that we remove
+users by username so, if two people were to be logged into the same
+account, some funky behavior might happen. As of now, the system
+doesn't enforce that but, it would be a good idea to!
+
+Finally, a user has to be able to send messages to other users. They
+do that by typing in their terminal. First, we create a message
+object with their username as the From attribute and the string
+they typed as the Message attribute. The message then get's appended
+to the rooms history and sent out to all of the other users.
+*/
 
 func (r *Room) Enter(sess ssh.Session, term *terminal.Terminal) {
 	u := User{Session: sess, Terminal: term}
@@ -51,6 +88,17 @@ func (r *Room) Leave(sess ssh.Session) {
 	r.Users = removeByUsername(r.Users, sess.User())
 }
 
+func (r *Room) SendMessage(from, message string) {
+
+	messageObj := Message{From: from, Message: message}
+	r.History = append(r.History, messageObj)
+	for _, u := range r.Users {
+		if (u.Session.User()) != from {
+			send(u, messageObj)
+		}
+	}
+}
+
 func removeByUsername(s []User, n string) []User {
 	var index int
 	for i, u := range s {
@@ -60,4 +108,9 @@ func removeByUsername(s []User, n string) []User {
 		}
 	}
 	return append(s[:index], s[index+1:]...)
+}
+
+func send(u User, m Message) {
+	raw := m.From + "> " + m.Message + "\n"
+	u.Terminal.Write([]byte(raw))
 }
